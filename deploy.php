d8d1c26b574e34432af6911198549965f0870017
douglasfresh <doug@Dougs-MacBook-Air.local>
2016-02-20 16:57:17 -0500

    .

--- deploy.php
+++ deploy.php
@@ -21,23 +21,7 @@ if(isset[$_GET["bg"]])
 
 require "less/lessphp/lessc.inc.php";
 
-function autoCompileLess($inputFile, $outputFile) {
-    // load the cache
-    $cacheFile = $inputFile.".cache";
-    if (file_exists($cacheFile)) {
-        $cache = unserialize(file_get_contents($cacheFile));
-    } else {
-        $cache = $inputFile;
-    }
-    $less = new lessc;
-    $less->setFormatter("compressed");
-    $newCache = $less->cachedCompile($cache);
-    if (!is_array($cache) || $newCache["updated"] > $cache["updated"]) {
-        file_put_contents($cacheFile, serialize($newCache));
-        file_put_contents($outputFile, $newCache['compiled']);
-    }
-}
-autoCompileLess($file, $file . '.css');
-header('Content-type: text/css');
-readfile('../' . $file . '.css');
+$less = new lessc;
+echo $less->compileFile($file);
+
 ?>
