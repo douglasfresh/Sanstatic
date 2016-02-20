0c52bc826417638b60a6f8091c3b29c5d684b198
douglasfresh <doug@Dougs-MacBook-Air.local>
2016-02-20 17:05:23 -0500

    .

--- deploy.php
+++ deploy.php
@@ -12,15 +12,15 @@ $file = "less/creative.less";
 // Create static index.html with Phantom
 // Push to GitHub
 
-if(isset[$_GET["font1"]]) 
+if(isset($_GET["font1"])) 
 	$font1 = $_GET["font1"];
-if(isset[$_GET["font2"]]) 
+if(isset($_GET["font2"]))
 	$font2 = $_GET["font2"];
-if(isset[$_GET["color1"]]) 
+if(isset($_GET["color1"]))
 	$color1 = $_GET["color1"];
-if(isset[$_GET["color2"]]) 
+if(isset($_GET["color2"]))
 	$color2 = $_GET["color2"];
-if(isset[$_GET["bg"]]) 
+if(isset($_GET["bg"])) 
 	$bg = $_GET["bg"];
 
 require "less/lessphp/lessc.inc.php";
