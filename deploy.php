601a02ada5532edfe626feaf3f60ed8b3b8d6219
douglasfresh <doug@Dougs-MacBook-Air.local>
2016-02-21 10:42:32 -0500

    .

--- deploy.php
+++ deploy.php
@@ -1,5 +1,7 @@
 <?php
 
+/* testing the forms
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -45,4 +47,36 @@ $phantom_script= dirname(__FILE__). '/js/get-website.js';
 $response =  exec ('phantomjs ' . $phantom_script);
 echo  htmlspecialchars($response);
 
+*/ 
+
 ?>
+
+<div ng-controller="ExampleController">
+  <form novalidate class="simple-form">
+    Name: <input type="text" ng-model="user.name" /><br />
+    E-mail: <input type="email" ng-model="user.email" /><br />
+    Gender: <input type="radio" ng-model="user.gender" value="male" />male
+    <input type="radio" ng-model="user.gender" value="female" />female<br />
+    <input type="button" ng-click="reset()" value="Reset" />
+    <input type="submit" ng-click="update(user)" value="Save" />
+  </form>
+  <pre>user = {{user | json}}</pre>
+  <pre>master = {{master | json}}</pre>
+</div>
+
+<script>
+  angular.module('formExample', [])
+    .controller('ExampleController', ['$scope', function($scope) {
+      $scope.master = {};
+
+      $scope.update = function(user) {
+        $scope.master = angular.copy(user);
+      };
+
+      $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+      };
+
+      $scope.reset();
+    }]);
+</script>
