f5f503ed98d3f4c36ac1d1a1cfee25580ac95274
douglasfresh <doug@Dougs-MacBook-Air.local>
2016-02-21 12:18:52 -0500

    .

--- deploy.php
+++ deploy.php
@@ -6,30 +6,46 @@ error_reporting(E_ALL);
 $variables = "less/variables.less";
 $input = "less/creative.less";
 $output = "css/creative.css";
+
 // Take in design specs
 // Create variables.less
 // Compile less
 // Create static index.html with Phantom
 // Push to GitHub
-if(isset($_GET["font1"])) 
+
+if(isset($_GET["font1"])) {
 	$font1 = $_GET["font1"];
-if(isset($_GET["font2"]))
+}
+
+if(isset($_GET["font2"])) {
 	$font2 = $_GET["font2"];
-if(isset($_GET["color1"]))
+}
+
+if(isset($_GET["color1"])) {
 	$color1 = "#" . $_GET["color1"];
-else
+}
+else {
 	$color1 = '#76ff03';
-if(isset($_GET["color2"]))
+}
+
+if(isset($_GET["color2"])) {
 	$color2 = $_GET["color2"];
-if(isset($_GET["bg"])) 
+}
+
+if(isset($_GET["bg"])) {
 	$bg = $_GET["bg"];
+}
+
+// Set the LESS variables
 $content = "@theme-primary:" . $color1 . "; @theme-dark:#222;";
-echo $content;
 file_put_contents($variables, $content);
+
+// Compile the LESS to CSS
 require "less/lessphp/lessc.inc.php";
 $less = new lessc;
 $less->compileFile($input, $output);
 
+// Create static index.html
 //$phantom_script= dirname(__FILE__). '/js/get-website.js'; 
 //$response =  exec ('phantomjs ' . $phantom_script);
 //echo  htmlspecialchars($response);
@@ -46,24 +62,24 @@ $less->compileFile($input, $output);
 <link rel="stylesheet" href="https://storage.googleapis.com/cdnsanstatic/css/animate.min.css" type="text/css">
 <link rel="stylesheet" href="http://sanstatic.com/site/css/creative.css" type="text/css">
 
-<div ng-app="myApp" ng-controller="formCtrl">
+<div ng-app="myApp" ng-controller="formCtrl" action="deploy.php">
   <form novalidate>
     Primary Color:<br>
-    <input type="text" ng-model="design.color1"><br>
+    <input type="text" ng-model="design.color1" name="color1"><br>
     Secondary Color:<br>
-    <input type="text" ng-model="design.color2"><br>
+    <input type="text" ng-model="design.color2" name="color2"><br>
     Heading Font:<br>
-    <input type="text" ng-model="design.font1"><br>
+    <input type="text" ng-model="design.font1" name="font1"><br>
     Paragraph Font:<br>
-    <input type="text" ng-model="design.font2"><br>
+    <input type="text" ng-model="design.font2" name="font2"><br>
     Background:<br>
-    <input type="text" ng-model="design.bg"><br>
+    <input type="text" ng-model="design.bg" name="bg"><br>
     <br><br>
     <button ng-click="reset()">RESET</button>
-    <button ng-click="deploy()">DEPLOY</button>
+    <button type="submit">DEPLOY</button>
   </form>
-  <p>design = {{design}}</p>
-  <p>contentful = {{contentful}}</p>
+  <p>brand = {{brand.toString();}}</p>
+  <p>contentful = {{contentful.toString();}}</p>
   <p>variables = <?php echo $content; ?></p>
 </div>
 
@@ -94,17 +110,14 @@ app.controller('formCtrl', ['$scope', '$q', '$http', function($scope, $q, $http)
    	   var entries = $q.when(client.entries({content_type: 'brand'}));
 
 	   entries.then(function(entries) {
-	      $scope.contentful = entries[0].fields;
+	   	  $scope.brand = new Array();
+	      $scope.brand = angular.fromJson(entries[0].fields);
 	      $scope.design = new Array();
 	      $scope.design["color1"] = entries[0].fields.colorScheme;
 	      $scope.design["font1"] = entries[0].fields.primaryFont;
 	      $scope.design["font2"] = entries[0].fields.secondaryFont;
 	      $scope.design["bg"] = entries[0].fields.picture;
 	   });
-   };
-
-   $scope.deploy = function() {
-      //
    };
 
 }]);
